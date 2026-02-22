260f60f46793569201f245601103b1cb6b8a9f87
Vinay Hugar <hugarvinay09@gmail.com>
2026-02-22 19:05:16 +0530

    Add package-lock.json

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app heading', () => {
-  render(<App />);
-  const heading = screen.getByRole('heading', { name: /react app built/i });
-  expect(heading).toBeInTheDocument();
-});
